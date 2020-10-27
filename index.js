b2f395b2936bcafdc0b072c40cb5427028e150f0
sagbeyeg <agbeyegbesisan@gmail.com>
2020-10-27 16:27:21 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+//for loop
+function writeCards(arr, event) {
+  let thankYou = []
+
+  for (let i = 0; i < arr.length; i++) {
+    thankYou.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYou
+}
+//test
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"))
+
+
+//while loop
+function countDown(integer) {
+  let i = integer;
+  while (i > -1) {
+    console.log(i--);
+  }
+}
+//test
+console.log(countDown(10))
